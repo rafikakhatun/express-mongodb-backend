767e99e5a83a91c32b62080844bcb45f16c32903
rafika khatun <rafikakhatun607@gmail.com>
2025-11-16 19:56:30 +0530

    feat: add cloudinary multer storage setup

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -8,3 +8,15 @@ cloudinary.config({
     api_secret: CLOUDINARY_API_SECRET,
 })
 
+const stroage = new CloudinaryStorage({
+    cloudinary:cloudinary,
+    params:{
+        folder:'user_profile',
+        allowed_formats:['jpg','png','jpeg'],
+    }
+
+})
+
+const upload = multer({storage:stroage});
+
+module.exports = upload;
