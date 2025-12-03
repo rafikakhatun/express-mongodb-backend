5bc179538e14738db3f31b959970d2a9da7bfc6a
rafika khatun <rafikakhatun607@gmail.com>
2025-12-03 23:21:12 +0530

    fix(api): correct deleteUser logic & add proper success/error responses

--- controllers/user.Controller.js
+++ controllers/user.Controller.js
@@ -93,9 +93,14 @@ const getAllUsers = async (req, res) => {
 const deleteUser = async (req, res) => {
     try {
         const user = await User.findByIdAndDelete(req.params.id);
+        if(user){
+            return res.status(404).json({message:'user not found'})
+        }
+        res.status(200).json({message:'User deleted successfully'})
         
        
     } catch (error) {
+        res.status(500).json({message:'Error deleting User',error:error.message})
 
     }
 
