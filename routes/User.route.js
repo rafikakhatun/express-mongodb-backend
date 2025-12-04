1581b519636e67af179836db1db0430164530970
rafika khatun <rafikakhatun607@gmail.com>
2025-12-04 22:43:12 +0530

    feat: add PATCH route to update user status by ID

--- routes/User.route.js
+++ routes/User.route.js
@@ -4,16 +4,21 @@ const {
     createUser,
     getAllUsers,
     deleteUser,
+    UpdateUserStatus,
 } = require('../controllers/user.controller');
 const upload = require('../config/cloudinary');
 
 // @route GET /api/users/all
 router.get('/all', getAllUsers);
-// @route POST /api/users/create(
+// @route POST /api/users/create
 router.post('/create',upload.single('profileImage'), createUser);
 
 // @route DELETE /api/users/delete/:id
 router.delete('/:id',deleteUser)
+// route active and inactive
+router.patch('/:id/status',UpdateUserStatus,) //http://localhost:5001/api/users/id/status
+
+
 
 
 module.exports = router;
